d431dee4cff2a3afbeb5dbb37cc7b9286a1491a8
Zoomball <107126392+Zoomba11@users.noreply.github.com>
2023-03-01 21:58:38 +0200

    Add files via upload

---
+++ LR11.cpp
@@ -0,0 +1,62 @@
+﻿//"разделяй и властвуй"
+//СЛОЖНОСТЬ: в лучшем случае : O(n* logn).; в худшем случае : O(n^2).
+
+#include <iostream>
+using namespace std;
+
+//*индекс опорного элемента
+int partition(int list[], int start, int pivot)
+{
+    int i = start;
+    //до того пока наш начальный элемент < опорного - мы выбираем последний эл. в списке
+    while (i < pivot)
+    {
+        if (list[i] > list[pivot] && i == pivot - 1)
+        {
+            swap(list[i], list[pivot]);
+            pivot--;
+        }
+
+        else if (list[i] > list[pivot])
+        {
+            swap(list[pivot - 1], list[pivot]);
+            swap(list[i], list[pivot]);
+            pivot--;
+        }
+
+        else i++;
+    }
+    return pivot;
+}
+
+void quickSort(int list[], int start, int end)
+{
+    if (start < end)
+    {
+        int pivot = partition(list, start, end);
+
+        quickSort(list, start, pivot - 1);
+        quickSort(list, pivot + 1, end);
+    }
+}
+
+int main()
+{
+    int list[6] = { 2, 12, 5, 48, 0, 4 };
+    cout << "Input array ...\n";
+    for (int i = 0; i < 6; i++)
+    {
+        cout << list[i] << "\t";
+    }
+
+    quickSort(list, 0, 6);
+
+    cout << "\n\nSorted array ... \n";
+    for (int i = 0; i < 6; i++)
+    {
+        cout << list[i] << "\t";
+    }
+
+    return 0;
+}
+
